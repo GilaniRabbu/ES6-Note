4093892bab1352133eb6cd7e45ea061e13b97b00
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-09-07 23:29:39 +0600

    Initial commit

---
+++ 05-Simple-For-Loop.js
@@ -0,0 +1,9 @@
+// ES6 Simple For Loop
+// The JavaScript for/of statement loops through the values of an iterable objects.
+// for/of lets you loop over data structures that are iterable such as Arrays, Strings, Maps, NodeLists, and more.
+
+var i;
+
+for (i = 0; i < 10; i = i++) {
+    console.log("Max");
+}
