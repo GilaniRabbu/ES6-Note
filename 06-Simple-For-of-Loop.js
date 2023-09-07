3872d648fc599dd4106e0f40c324c2258a4dfd94
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-09-07 23:29:52 +0600

    Initial commit

---
+++ 06-Simple-For-of-Loop.js
@@ -0,0 +1,9 @@
+// ES6 Simple For of Loop
+// The JavaScript for/of statement loops through the values of an iterable objects.
+// for/of lets you loop over data structures that are iterable such as Arrays, Strings, Maps, NodeLists, and more.
+
+var myCount = ["one", "two", "three"];
+
+for (let newCount of myCount) {
+    console.log(newCount);
+}
