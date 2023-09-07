dc6c4407d4299d5c24a0848114700287eb944b6c
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-09-07 23:29:00 +0600

    Initial commit

---
+++ 02-Spread-Operator.js
@@ -0,0 +1,11 @@
+// ES6 Spread Operator
+// Spread the contents of an iterable object (such as an array or object) into another iterable object.
+// The spread operator is denoted by three dots (...)
+
+let Asia = ["Bangladesh", "Oman", "Japan"];
+
+let Africa = [...Asia, "Tunisia", "Algeria", "Egypt"];
+
+// Output ['Bangladesh', 'Oman', 'Japan', 'Tunisia', 'Algeria', 'Egypt'];
+
+console.log(Africa);
