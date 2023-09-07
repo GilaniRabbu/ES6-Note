1e97897cd3fc6747c3f573db0736ed0439321e53
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-09-07 23:28:45 +0600

    Initial commit

---
+++ 01-Strict-Mode.js
@@ -0,0 +1,12 @@
+// ES6 The Strict Mode
+// Preventing you from using undeclared variables.
+// Removes some of the JavaScript silent errors by changing them to throw errors.
+// Fixed the mistakes, That is difficult for JS Engine to overcome.
+
+"use strict";
+
+MyName();
+
+function MyName() {
+    name = "TOUFIQ GILANI";
+}
